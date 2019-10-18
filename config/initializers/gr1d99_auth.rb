747fb8c49868591b2eb869425da8cf027c1c1dd8
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2019-10-18 14:33:42 +0300

    use encrypted credentials

--- config/initializers/gr1d99_auth.rb
+++ config/initializers/gr1d99_auth.rb
@@ -1,7 +1,7 @@
 Gr1d99Auth.configure do |config|
-  config.jwt_key    = "simple-key"
+  config.jwt_key    = Rails.application.credentials.secret_key_base
   config.jwt_verify = true
-  config.jwt_algorithm = 'HS512'
-  config.jwt_exp = 36_000
+  config.jwt_algorithm = Rails.application.credentials.JWT[:algorithm]
+  config.jwt_exp = Rails.application.credentials.JWT[:exp]
   config.time_zone = "Africa/Nairobi"
 end
